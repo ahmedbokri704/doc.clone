69f6a753f1dbddac8ca06d9cdfc87c15e1540716
ahmedbokri <ahmedbokri25@gmail.com>
2020-04-14 00:04:01 +0100

    first commit

---
+++ src/component/section4_1.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './App.css';
+
+class Section4_1 extends Component {
+    render() {
+        return (
+<div className="section4_1">
+<h5>Improve the lives of millions. <br/>Change yours forever</h5>
+<p>More than 1000 team mates share our same vision, goals and passion.<br/>With offices in Warsaw, Barcelona, Istanbul, Rome, Mexico City and <br/>Curitiba, our search for great talent never stops.</p></div>
+        )}}
+export default Section4_1;
--- src/component/section5.js
+++ src/component/section5.js
@@ -4,11 +4,11 @@ import './App.css';
 
 const Section5 = props => {
         return (
-         <section id="section5" class="container">
+         <section id="section5" className="container">
           {props.worldcard.map((element, index) => (
-           <div class="card6">
+           <div className="card6">
              <a href=""> <img src={element.image}/></a>
-           <div class="card-title">
+           <div className="card-title">
             <p>{element.p}</p>
              <button>See openings</button>
            </div>
